759abea7f5116e160907eddfc0c6b59c2821ca32
InceptionSE <85175281+InceptionSE@users.noreply.github.com>
2022-06-15 00:19:37 +0700

    add client

---
+++ pizza/client/src/components/mainpages/Pages.js
@@ -0,0 +1,48 @@
+
+      
+       import React, {useContext} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Products from './products/Products'
+import DetailProduct from './detailProduct/DetailProduct'
+import Login from './auth/Login'
+import Register from './auth/Register'
+import OrderHistory from './order/OrderHistory'
+import OrderDetails from './order/OrderDetails'
+import Cart from './cart/Cart'
+import NotFound from './utils/not_found/NotFound'
+import Categories from './categories/Categories'
+import CreateProduct from './createProduct/CreateProduct'
+
+import {GlobalState} from '../../GlobalState'
+
+
+function Pages() {
+    const state = useContext(GlobalState)
+    const [isLogged] = state.userAPI.isLogged
+    const [isAdmin] = state.userAPI.isAdmin
+
+
+    return (
+        <Routes>
+            <Route path="/" element={<Products/>} />
+            <Route path="/detail/:id" element={<DetailProduct/>} />
+
+            <Route path="/login" element={isLogged ? <NotFound/> : <Login/>} />
+            <Route path="/register" element={isLogged ? <NotFound/> :< Register/>} />
+          
+            <Route path="/category" element={isAdmin ? <Categories/> : <NotFound/>} />
+
+            <Route path="/create_product" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
+            <Route path="/edit_product/:id" element={isAdmin ? <CreateProduct/> : <NotFound/>} />
+
+            <Route path="/order" element={isLogged ? <OrderHistory/> : <NotFound/>} />
+            <Route path="/order/:id" element={isLogged ? <OrderDetails/> : <NotFound/>} />
+
+            <Route path="/cart" element={<Cart/>} />
+
+            <Route path="*" element={<NotFound/>} />
+        </Routes>
+    )
+}
+
+export default Pages
